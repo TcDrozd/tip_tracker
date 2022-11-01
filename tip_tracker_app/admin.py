f02c8d88eb8a25c00678c9c3fef39447c9534942
tcdrozd <trcharlesd@gmail.com>
2022-11-01 16:15:36 -0400

    Started building some models for the project

--- tip_tracker_app/admin.py
+++ tip_tracker_app/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
 
 # Register your models here.
+from tip_tracker_app.models import Tip_Entry
+
+admin.site.register(Tip_Entry)
--- tip_tracker_app/models.py
+++ tip_tracker_app/models.py
@@ -1,3 +1,31 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Tip_Type(models.Model):
+    """ The type of tip to be entered """
+    TipType = models.TextChoices('Cash', 'Credit Card')
+    name = models.CharField(max_length=60)
+    tip = models.CharField(blank=True,
+                           choices = TipType.choices,
+                           max_length=10,
+                           )
+    class Meta:
+        verbose_name_plural = 'tip types'
+
+class Tip_Entry(models.Model):
+    """ An entry into the tip logger of type tip_type """
+    tip_type = models.ForeignKey(
+        Tip_Type,
+        on_delete=models.CASCADE,
+        default=0
+    )
+    amt = models.FloatField()
+    date_added = models.DateField(auto_now_add=True)
+    class Meta:
+        verbose_name_plural = 'tip entries'
+
+    def __float__(self):
+        """ Return a string representation of the model. """
+        return self.amt
